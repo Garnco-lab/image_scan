d1bd368d988dae410f7b2e5e04d6b2d82296864a
Steven Deleon <51217148+Garnco-lab@users.noreply.github.com>
2022-04-03 21:55:39 -0700

    added model loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,30 @@
+import { useState, useEffect } from "react";
+import * as mobilenet from "@tensorflow-models/mobilenet";
+
 function App() {
+  const [isModelLoading, setIsModelLoading] = useState(false);
+  const [model, setModel] = useState(null);
+
+  const loadModel = async () => {
+    setIsModelLoading(true);
+    try {
+      const model = await mobilenet.load();
+      setModel(model);
+      setIsModelLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsModelLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadModel();
+  }, []);
+
+  if (isModelLoading) {
+      return <h2>Model Loading...</h2>
+  }
+
   return (
     <div className="App">
       <h1>Image Identification</h1>
